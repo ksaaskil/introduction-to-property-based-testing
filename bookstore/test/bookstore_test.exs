d164ecc531fce77a91ef383befd213793354bbf7
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-24 15:00:58 +0200

    Work on test module.

--- bookstore/test/bookstore_test.exs
+++ bookstore/test/bookstore_test.exs
@@ -1,6 +1,33 @@
 defmodule BookstoreTest do
   use ExUnit.Case
+  use PropCheck
+  use PropCheck.StateM
   doctest Bookstore
+
+  def title() do
+    let s <- utf8() do
+      elements([s, String.to_charlist(s)])
+    end
+  end
+
+  def author() do
+    let s <- utf8() do
+      elements([s, String.to_charlist(s)])
+    end
+  end
+
+  def isbn() do
+    let(
+      isbn <- [
+        oneof(['978', '979']),
+        let(x <- range(0, 9999), do: to_charlist(x)),
+        let(x <- range(0, 999), do: to_charlist(x)),
+        frequency([{10, [range(?0, ?9)]}, {1, 'X'}])
+      ]
+    ) do
+      to_string(Enum.join(isbn, "-"))
+    end
+  end
 
   test "greets the world" do
     assert Bookstore.hello() == :world
