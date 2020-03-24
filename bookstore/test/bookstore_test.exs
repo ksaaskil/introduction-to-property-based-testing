397f4291b417c29c476428d4425fbe55ad0ec6e0
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-24 15:09:56 +0200

    Working stateful test.

--- bookstore/test/bookstore_test.exs
+++ bookstore/test/bookstore_test.exs
@@ -29,6 +29,48 @@ defmodule BookstoreTest do
     end
   end
 
+  property "bookstore stateful ops", [:verbose] do
+    forall cmds <- commands(__MODULE__) do
+      {:ok, apps} = Application.ensure_all_started(:bookstore)
+      Bookstore.DB.setup()
+      {history, state, result} = run_commands(__MODULE__, cmds)
+      Bookstore.DB.teardown()
+      for app <- apps, do: Application.stop(app)
+
+      (result == :ok)
+      |> aggregate(command_names(cmds))
+      |> when_fail(
+        IO.puts("""
+        History: #{inspect(history)}
+        State: #{inspect(state)}
+        Result: #{inspect(result)}
+        """)
+      )
+    end
+  end
+
+  def initial_state(), do: %{}
+
+  def command(_state) do
+    oneof([
+      {:call, Bookstore.DB, :add_book, [isbn(), title(), author(), 1, 1]},
+      {:call, Bookstore.DB, :find_book_by_title, [title()]}
+    ])
+  end
+
+  def precondition(_state, {:call, _mod, _fun, _args}) do
+    true
+  end
+
+  def postcondition(_state, {:call, _mod, _fun, _args}, _res) do
+    true
+  end
+
+  def next_state(state, _res, {:call, _mod, _fun, _args}) do
+    new_state = state
+    new_state
+  end
+
   test "greets the world" do
     assert Bookstore.hello() == :world
   end
