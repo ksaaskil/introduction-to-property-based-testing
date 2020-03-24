ad4db906a9c94505b0f846e820db3eda760a3033
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-24 08:41:22 +0200

    Add sort examples.

--- python-hypothesis/test_hypothesis.py
+++ python-hypothesis/test_hypothesis.py
@@ -61,3 +61,24 @@ def test_look_tuples_work_too(t):
     assert len(t) == 2
     assert isinstance(t[0], bool)
     assert isinstance(t[1], str)
+
+
+def my_sort(l):
+    return sorted(l)
+
+# Example-based test
+
+
+def test_mysort():
+    l = [5, 3, 2, 1, 6]
+    l_sorted = my_sort(l)
+    assert l_sorted == [1, 2, 3, 5, 6]
+
+
+@given(l=some.lists(some.integers()))
+def test_my_sort(l):
+    l_sorted = my_sort(l)
+    assert len(l_sorted) == len(l)
+    assert set(l_sorted) == set(l)
+    for i in range(len(l) - 1):
+        assert l_sorted[i] <= l_sorted[i+1]
