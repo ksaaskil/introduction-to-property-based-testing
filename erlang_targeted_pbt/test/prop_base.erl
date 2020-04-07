7ead69f4907e8a24e5af21d5e45249734a04690e
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-04-07 10:29:32 +0300

    Add test for biggest.

--- erlang_targeted_pbt/test/prop_base.erl
+++ erlang_targeted_pbt/test/prop_base.erl
@@ -6,14 +6,21 @@
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
 prop_biggest() ->
-    ?FORALL(Type, (term()), begin boolean(Type) end).
+    ?FORALL(List, non_empty(list(integer())),
+        begin
+            biggest(List) =:= lists:last(lists:sort(List))
+        end).
 
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
 %%%%%%%%%%%%%%%
-boolean(_) -> true.
+biggest([Head | Tail]) ->
+    biggest(Tail, Head).
 
+biggest([], Biggest) -> Biggest;
+biggest([Head | Tail], Biggest) when Head >= Biggest -> biggest(Tail, Head);
+biggest([Head | Tail], Biggest) when Head < Biggest -> biggest(Tail, Biggest).
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
 %%%%%%%%%%%%%%%%%%
-mytype() -> term().
+
