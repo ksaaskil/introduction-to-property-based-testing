696ad3cfa0d4c26f8b33db0cce42c50f65e1e623
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-04-14 11:04:46 +0300

    Contrived example of targeted PBT.

--- python-hypothesis/test_targeted_pbt.py
+++ python-hypothesis/test_targeted_pbt.py
@@ -1,13 +1,13 @@
-from hypothesis import given
+from hypothesis import given, target, settings, Phase, Verbosity
 import hypothesis.strategies as some
 import functools
-
-
-def log(*args, **kwargs):
-    print(*args, **kwargs)
+import math
 
 
 def with_logging(logger):
+    """Simple helper for understanding what's going on in targeted PBT.
+    """
+
     def wrapper(func):
         @functools.wraps(func)
         def f(*args, **kwargs):
@@ -27,8 +27,30 @@ def with_logging(logger):
     return wrapper
 
 
-@given(s=some.integers())
-@with_logging(log)
-def test_string2(s):
-    # assert isinstance(s, str)
-    pass
+# @with_logging(print)
+
+
+@given(d=some.floats().filter(lambda x: abs(x) < 100))
+@settings(
+    max_examples=1000,
+    verbosity=Verbosity.verbose,
+    phases=[
+        Phase.generate,
+        Phase.target,
+        Phase.shrink,
+    ],  # Skip Phase.reuse to not run previous counter-examples
+)
+def test_targeting_square_loss(d):
+    """Contrived example of targeting properties.
+    """
+    # Assume this value triggers a bug
+    target_value = 42
+
+    should_fail = abs(d - target_value) < 0.5
+    if should_fail:
+        print("Failing with value {}".format(d))
+        raise Exception("Critically close to {}, got {}".format(target_value, d))
+
+    # Target the value
+    loss = math.pow((d - target_value), 2.0)
+    target(-loss)
